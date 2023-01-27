7052ebf1ef72c493878aea8cc584e410f5a26ab4
Nigel Christiaans <nigel.christiaans@hva.nl>
2023-01-27 21:31:44 +0100

    Inital edit for new database layout

--- commands/setup.js
+++ commands/setup.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, ChannelType } = require("discord.js");
 const {initializeApp} = require("firebase/app");
-const {getFirestore, doc, updateDoc} = require("firebase/firestore");
+const {getFirestore, doc, updateDoc, setDoc} = require("firebase/firestore");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,6 +15,10 @@ module.exports = {
               .setDescription('Category to create tickets in')
               .setRequired(true)
               .addChannelTypes(ChannelType.GuildCategory))
+      .addChannelOption(channel =>
+          channel.setName('logs')
+                .setDescription('Channel for logs.')
+                .setRequired(true))
     .addRoleOption(role =>
         role.setName('staffrole')
             .setDescription('Role for staff')
@@ -22,11 +26,23 @@ module.exports = {
       .addRoleOption(role =>
           role.setName('moderatorrole')
               .setDescription('Role for moderators')
-              .setRequired(false))
-      .addStringOption(option =>
-          option.setName('prefix')
-                .setDescription('Prefix for the bot')
+              .setRequired(true))
+      .addRoleOption(role =>
+            role.setName('muterole')
+                .setDescription('Role for muted users')
+                .setRequired(true))
+      .addRoleOption(role =>
+            role.setName('helperrole')
+                .setDescription('Role for helpers')
                 .setRequired(false))
+      .addBooleanOption(option =>
+          option.setName('AllowHelpersInTickets')
+            .setDescription('Allow helpers to see tickets (default: false)')
+            .setRequired(false))
+      .addBooleanOption(option =>
+          option.setName('AllowModeratorToBan')
+            .setDescription('Allow moderators to ban users (default: false)')
+            .setRequired(false))
       .setDefaultMemberPermissions(0)
       .setDMPermission(false),
   async execute(interaction) {
@@ -37,44 +53,7 @@ module.exports = {
           ephemeral: true,
         });
       }
-        const category = interaction.options.getChannel("ticketcategory");
-        const staffRole = interaction.options.getRole("staffrole");
-        const moderatorRole = interaction.options.getRole("moderatorrole");
-        const adminRole = interaction.options.getRole("adminrole");
-        const prefix = interaction.options.getString("prefix");
 
-        const firebaseConfig = {
-            apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
-            authDomain: "agile-bot-2003.firebaseapp.com",
-            projectId: "agile-bot-2003",
-            storageBucket: "agile-bot-2003.appspot.com",
-            messagingSenderId: "1014532189070",
-            appId: "1:1014532189070:web:5a0c45449e27bc068312df"
-        };
-        const app = initializeApp(firebaseConfig);
-        const db = getFirestore(app);
-
-        const docRef = doc(db, "Guilds", interaction.guild.id);
-
-        await updateDoc(docRef, {
-            ticketCat: category.id,
-            setup: true,
-            prefix: ((prefix) ? prefix : "/"),
-            staffRoleId: ((staffRole) ? staffRole.id : null),
-            adminRoleId: adminRole.id,
-            moderatorRoleId: ((moderatorRole) ? moderatorRole.id : null),
-        }).then(() => {
-            interaction.reply({
-                content: `Setup complete!`,
-                ephemeral: true,
-            });
-        }).catch((error) => {
-            console.error("Error writing document: ", error);
-            interaction.reply({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        });
 
     } catch (error) {
         console.error(error);
@@ -85,3 +64,85 @@ module.exports = {
     }
   },
 };
+
+
+//TODO: add the values to the promises!
+async function pushSetupDataToFirebase(interaction){
+    const category = interaction.options.getChannel("ticketcategory");
+    const staffRole = interaction.options.getRole("staffrole");
+    const moderatorRole = interaction.options.getRole("moderatorrole");
+    const adminRole = interaction.options.getRole("adminrole");
+    const muteRole = interaction.options.getRole("muterole");
+    const helperRole = interaction.options.getRole("helperrole");
+    const allowHelpersInTickets = interaction.options.getBoolean("AllowHelpersInTickets");
+    const allowModeratorToBan = interaction.options.getBoolean("AllowModeratorToBan");
+    const logs = interaction.options.getChannel("logs");
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
+        authDomain: "agile-bot-2003.firebaseapp.com",
+        projectId: "agile-bot-2003",
+        storageBucket: "agile-bot-2003.appspot.com",
+        messagingSenderId: "1014532189070",
+        appId: "1:1014532189070:web:5a0c45449e27bc068312df"
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const createTicketStatsRef = doc(db, "Guilds", guild.id, "stats", "tickets");
+    const createModStatsRef = doc(db, "Guilds", guild.id, "stats", "moderation");
+    const createSettingsCatagoriesRef = doc(db, "Guilds", guild.id, "settings", "catagories");
+    const createSettingsAccessRef = doc(db, "Guilds", guild.id, "settings", "access");
+    const createSettingsRolesRef = doc(db, "Guilds", guild.id, "settings", "roles");
+    const createSettingsChannelsRef = doc(db, "Guilds", guild.id, "settings", "channels");
+    const createfeaturesEnabledSettingsRef = doc(db, "Guilds", guild.id, "settings", "featuresEnabled");
+    const createSetupRef = doc(db, "Guilds", guild.id);
+
+    const promise1 = setDoc(createTicketStatsRef, {
+        numbTicketsOpend: 0,
+        numbTicketsClosed: 0,
+    });
+
+    const promise2 = setDoc(createModStatsRef, {
+        bans: 0,
+        kicks: 0,
+        mutes: 0,
+        clears: 0,
+        clearMessages: 0,
+    });
+
+    const promise3 = setDoc(createSettingsCatagoriesRef, {
+        ticketId: "none"
+    });
+
+    const promise4 = setDoc(createSettingsAccessRef, {
+        helperTicket: false,
+        moderatorBan: false
+    });
+
+    const promise5 = setDoc(createSettingsRolesRef, {
+        adminRoleId: "none",
+        moderatorRoleId: "none",
+        helperRoleId: "none",
+        mutedRoleId: "none",
+        staffRoleId: "none",
+    });
+
+    const promise6 = setDoc(createSettingsChannelsRef, {
+        logsId: "none"
+    });
+
+    const promise7 = setDoc(createfeaturesEnabledSettingsRef, {
+        logs: false,
+        tickets: true
+    })
+
+    const promise8 = setDoc(createSetupRef, {
+        setup: false,
+    });
+
+    await Promise.all([promise1, promise2, promise3, promise4, promise5, promise6, promise7, promise8]).catch((error) => {
+        channel.send("Error setting up database, please try again later. If this error persists, please contact the developer.")
+        return console.error("Error writing document: ", error);
+    });
+}
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,51 +5,86 @@ const { initializeApp } = require ("firebase/app");
 module.exports = {
     name: Events.GuildCreate,
     async execute(guild) {
-        const firebaseConfig = {
-            apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
-            authDomain: "agile-bot-2003.firebaseapp.com",
-            projectId: "agile-bot-2003",
-            storageBucket: "agile-bot-2003.appspot.com",
-            messagingSenderId: "1014532189070",
-            appId: "1:1014532189070:web:5a0c45449e27bc068312df"
-        };
-        const app = initializeApp(firebaseConfig);
-        const db = getFirestore(app)
-
-        await setDoc(doc(db, "Guilds", guild.id), {
-            ticketCat: null,
-            setup: false,
-            prefix: "/",
-            staffRoleId: null,
-            adminRoleId: null,
-            moderatorRoleId: null
-        });
-
-        const createDocumentRef = doc(db, "Guilds", guild.id, "stats", "tickets");
-        const createModStatsRef = doc(db, "Guilds", guild.id, "stats", "moderation");
-
-        await setDoc(createDocumentRef, {
-            numbTicketsOpend: 0,
-            numbTicketsClosed: 0,
-        });
-        await setDoc(createModStatsRef, {
-            bans: 0,
-            kicks: 0,
-            mutes: 0,
-            clears: 0,
-            clearMessages: 0,
-        });
-
         const channel = guild.channels.cache.find(
             (c) => c.type === ChannelType.GuildText && c.permissionsFor(guild.members.me).has("SEND_MESSAGES")
         );
         // Do something with the channel
-
+        await setupDatabase(guild, channel);
         if(channel){
-            channel.send("Thanks for adding me to your server! To get started, use the /setup command in a channel. Please note this is required for the bot to function correctly! \n If you need help, use the /help command. \nIf you have already had this bot in the past you need to set it up again.")
+            channel.send("Thanks for adding me to your server! **To get started, use the /setup command in a channel.** Please note this is required for the bot to function correctly! \nIf you need help, use the /help command. \nIf you have already had this bot in the past you still need to set it up again.")
         } else {
             console.log(`No channel found to send message to in ${guild.name}!`)
         }
 
     },
 };
+
+async function setupDatabase(guild, channel) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
+        authDomain: "agile-bot-2003.firebaseapp.com",
+        projectId: "agile-bot-2003",
+        storageBucket: "agile-bot-2003.appspot.com",
+        messagingSenderId: "1014532189070",
+        appId: "1:1014532189070:web:5a0c45449e27bc068312df"
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app)
+
+    const createTicketStatsRef = doc(db, "Guilds", guild.id, "stats", "tickets");
+    const createModStatsRef = doc(db, "Guilds", guild.id, "stats", "moderation");
+    const createSettingsCatagoriesRef = doc(db, "Guilds", guild.id, "settings", "catagories");
+    const createSettingsAccessRef = doc(db, "Guilds", guild.id, "settings", "access");
+    const createSettingsRolesRef = doc(db, "Guilds", guild.id, "settings", "roles");
+    const createSettingsChannelsRef = doc(db, "Guilds", guild.id, "settings", "channels");
+    const createfeaturesEnabledSettingsRef = doc(db, "Guilds", guild.id, "settings", "featuresEnabled");
+    const createSetupRef = doc(db, "Guilds", guild.id);
+
+    const promise1 = setDoc(createTicketStatsRef, {
+        numbTicketsOpend: 0,
+        numbTicketsClosed: 0,
+    });
+
+    const promise2 = setDoc(createModStatsRef, {
+        bans: 0,
+        kicks: 0,
+        mutes: 0,
+        clears: 0,
+        clearMessages: 0,
+    });
+
+    const promise3 = setDoc(createSettingsCatagoriesRef, {
+        ticketId: "none"
+    });
+
+    const promise4 = setDoc(createSettingsAccessRef, {
+        helperTicket: false,
+        moderatorBan: false
+    });
+
+    const promise5 = setDoc(createSettingsRolesRef, {
+        adminRoleId: "none",
+        moderatorRoleId: "none",
+        helperRoleId: "none",
+        mutedRoleId: "none",
+        staffRoleId: "none",
+    });
+
+    const promise6 = setDoc(createSettingsChannelsRef, {
+        logsId: "none"
+    });
+
+    const promise7 = setDoc(createfeaturesEnabledSettingsRef, {
+        logs: false,
+        tickets: true
+    })
+
+    const promise8 = setDoc(createSetupRef, {
+            setup: false,
+        });
+
+    await Promise.all([promise1, promise2, promise3, promise4, promise5, promise6, promise7, promise8]).catch((error) => {
+        channel.send("Error setting up database, please try again later. If this error persists, please contact the developer.")
+        return console.error("Error writing document: ", error);
+    });
+}
