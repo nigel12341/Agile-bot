7b4e4c8ef0b73b9734f27c3a70c94e035c1afacd
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-21 11:07:19 +0100

    Added firestore stats update

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
+const {initializeApp} = require("firebase/app");
+const {getFirestore, doc, updateDoc, increment} = require("firebase/firestore");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,6 +19,17 @@ module.exports = {
       .setDMPermission(false)
       .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
   async execute(interaction) {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
+      authDomain: "agile-bot-2003.firebaseapp.com",
+      projectId: "agile-bot-2003",
+      storageBucket: "agile-bot-2003.appspot.com",
+      messagingSenderId: "1014532189070",
+      appId: "1:1014532189070:web:5a0c45449e27bc068312df"
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const moderationStatsRef = doc(db, "Guilds", interaction.guild.id, "stats", "moderation");
     const target = interaction.options.getUser('target');
     const row = new ActionRowBuilder()
         .addComponents(
@@ -40,6 +53,9 @@ module.exports = {
       if (i.customId === 'yesBanButton') {
         await i.deferUpdate();
         if(!interaction.guild.members.cache.get(target.id).bannable) return i.editReply({ content: '❎ I cannot ban this member.\nThis is possibly caused by the permissions setup incorrectly.', components: []});
+        await updateDoc(moderationStatsRef, {
+          bans: increment(1)
+        });
         await i.guild.members.ban(target, { reason: interaction.options.getString('reason') || `Kicked by ${interaction.user.username}` });
         await i.editReply({ content: `✅ ${target.username} has been banned.`, components: [] });
       }
