b778656a9d76832d0f1e98bbd8937383f2ff1bb4
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-02-21 01:35:15 +0100

    Bad words filter coming along

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,6 +8,7 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return;
         if (message.author.me) return;
+        if(message.channel.type === "DM") return;
         const firebaseConfig = {
             apiKey: "AIzaSyAsFPkrCVt2w5vjzZ-JaajZvIjwSLfRwwE",
             authDomain: "agile-bot-2003.firebaseapp.com",
@@ -18,18 +19,39 @@ module.exports = {
         };
         const app = initializeApp(firebaseConfig);
         const db = getFirestore(app);
-        const badWordsRef = doc(db, "Guilds", message.guild.id, "settings", "badWordsFilter");
-
-        const badWords = await getDoc(badWordsRef);
-        const badWordsList = badWords.data().badWords;
-
-        if (!badWords.exists()) return;
-        if (badWords.data().toggle === false) return;
-        if (badWordsList === undefined || badWordsList.length === 0) return;
-        if (badWordsList.includes(message.content.toLowerCase())) {
-            message.channel.send({
-                content: `Please don't use bad words ${message.author}!`});
-            message.delete();
-        }
+        await checkBadWords(message, db);
+        await checkQuestions(message, db);
     },
 };
+
+async function checkBadWords(message, db) {
+
+    const badWordsRef = doc(db, "Guilds", message.guild.id, "settings", "badWordsFilter");
+
+    const badWords = await getDoc(badWordsRef);
+    const badWordsList = badWords.data().badWords;
+
+    if (!badWords.exists()) return;
+    if (badWords.data().toggle === false) return;
+    if (badWordsList === undefined || badWordsList.length === 0) return;
+    if (badWordsList.includes(message.content.toLowerCase())) {
+        message.channel.send({
+            content: `Please don't use bad words ${message.author}!`});
+        message.delete();
+    }
+}
+async function checkQuestions(message, db) {
+    const questionsRef = doc(db, "Guilds", message.guild.id, "settings", "autoAnswer");
+
+    const questions = await getDoc(questionsRef);
+    const badWordsList = badWords.data().badWords;
+
+    if (!badWords.exists()) return;
+    if (badWords.data().toggle === false) return;
+    if (badWordsList === undefined || badWordsList.length === 0) return;
+    if (badWordsList.includes(message.content.toLowerCase())) {
+        message.channel.send({
+            content: `Please don't use bad words ${message.author}!`});
+        message.delete();
+    }
+}
